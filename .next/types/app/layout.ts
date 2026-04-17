f35d32fd65f9af0bc54579c8313428814e234c7d
Connor Toolson <connortoolson@MacBook-Air-2.local>
2026-04-17 13:54:20 -0700

    fix page.tsx syntax error

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/connortoolson/my-gvc-quest/app/layout.tsx
+// File: /Users/connortoolson/gvc-quests/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/connortoolson/my-gvc-quest/app/page.tsx
+// File: /Users/connortoolson/gvc-quests/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
